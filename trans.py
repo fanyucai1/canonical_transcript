d8b0bb32a4212b543e04b49abd8f94d768a29f46
fanyucai1 <fanyucai1@126.com>
2021-08-25 16:07:48 +0800

    no

--- trans.py
+++ trans.py
@@ -1,25 +1,23 @@
-
-
 map,line={},""
 infile=open("MSK.txt","r")
 for line in infile:
     line=line.strip()
-    map[line.split()[0]]=line.split()[1]
-    print(line.split()[0],"\t",line.split()[1])
+    map[line.split(",")[0]]=line.split(",")[1]
+    print("%s,%s"%(line.split(",")[0],line.split(",")[1]))
 infile.close()
 
-infile=open("/Users/fanyucai/Desktop/MANE.GRCh38.v0.95.select_ensembl_genomic.gff","r")#https://ftp.ncbi.nlm.nih.gov/refseq/MANE/MANE_human/current/
-gene_name,transript="",""
+infile=open("/Users/fanyucai/Downloads/MANE.GRCh38.v0.95.select_ensembl_genomic.gff","r")#https://ftp.ncbi.nlm.nih.gov/refseq/MANE/MANE_human/current/
 for line in infile:
     line=line.strip()
     if not line.startswith("#") and line.split("\t")[2]=="transcript":
         array=line.split(";")
+        gene_name, transript = "", ""
         for i in range(0,len(array)):
             if array[i].startswith('gene_name'):
                 gene_name=array[i].split("=")[1]
             if array[i].startswith('Dbxref=RefSeq:'):
                 transript=array[i].split(':')[1].split('.')[0]
-            if not gene_name in map:
-                map[gene_name]=transript
-                print(gene_name,"\t",transript)
+        if not gene_name in map:
+            map[gene_name]=transript
+            print("%s,%s"%(gene_name,transript))
 infile.close()
